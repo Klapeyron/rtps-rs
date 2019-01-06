0f33cbb30b0c3155a91ddf24931d120ddec37b8f
Piotr Gumienny <piotrgumienny@o2.pl>
2019-01-07 00:35:58 +0100

    Time_t::fraction represented correctly and tested

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,8 @@
 extern crate bit_set;
 extern crate bit_vec;
 extern crate bytes;
-#[macro_use]
-extern crate log;
+//#[macro_use]
+//extern crate log;
 extern crate tokio;
 #[macro_use]
 extern crate speedy_derive;
